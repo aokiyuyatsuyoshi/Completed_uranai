ceee34dd944148189b19afe0f877934a656af856
aokiyuyatsuyoshi <rainarrow0114@ezweb.ne.jp>
2020-03-21 02:15:58 +0900

    Initial Commit

---
+++ uranai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uranai
+//
+//  Created by Yuya Aoki on 2020/03/21.
+//  Copyright © 2020 Yuya Aoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

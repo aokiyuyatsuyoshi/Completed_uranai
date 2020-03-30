c3c1f6117d1658849734cd0650bb868aea78809c
aokiyuyatsuyoshi <rainarrow0114@ezweb.ne.jp>
2020-03-30 15:12:13 +0900

    first commit

---
+++ uranai/other_day_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  other_day_ViewController.swift
+//  uranai
+//
+//  Created by Yuya Aoki on 2020/03/25.
+//  Copyright © 2020 Yuya Aoki. All rights reserved.
+//
+
+import UIKit
+
+class other_day_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ uranai/resultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  resultViewController.swift
+//  uranai
+//
+//  Created by Yuya Aoki on 2020/03/21.
+//  Copyright © 2020 Yuya Aoki. All rights reserved.
+//
+
+import UIKit
+
+class resultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
